3c257504b43ae6ed5642847a94d6238ad6b911b8
Everett Yeaw <evyeaw@gmail.com>
2020-10-14 05:57:03 -0400

    setting block sizes with scale

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+
+// Calculate size of canvas from constants.
+ctx.canvas.width = COLS * BLOCK_SIZE;
+ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+// Scale blocks
+ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
 let board = new Board();
 
 function play() {
