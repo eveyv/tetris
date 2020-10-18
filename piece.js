b7a963947ed81a8b012db95f5f4ad02575cf40b9
Everett Yeaw <evyeaw@gmail.com>
2020-10-18 17:01:24 -0400

    functional version 1

--- piece.js
+++ piece.js
@@ -1,29 +1,46 @@
 class Piece {
+  x;
+  y;
+  color;
+  shape;
+  ctx;
+  typeId;
 
   constructor(ctx) {
     this.ctx = ctx;
-    this.color = 'blue';
-    this.shape = [
-      [2, 0, 0],
-      [2, 2, 2],
-      [0, 0, 0]
-    ];
-//origin
-    this.x = 3;
-    this.y = 0;
-  };
+    this.spawn();
+  }
 
-draw() {
-  this.ctx.fillStyle = this.color;
-  this.shape.forEach((row, y) => {
-    row.forEach((value, x) => {
-      // this.x, this.y gives the left upper position of the shape
-      // x, y gives the position of the block in the shape
-      // this.x + x is the position of the block on the board
-      if (value > 0) {
-        this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
-      }
+  spawn() {
+    this.typeId = this.randomizeTetrominoType(COLORS.length - 1);
+    this.shape = SHAPES[this.typeId];
+    this.color = COLORS[this.typeId];
+    this.x = 0;
+    this.y = 0;
+  }
+
+  draw() {
+    this.ctx.fillStyle = this.color;
+    this.shape.forEach((row, y) => {
+      row.forEach((value, x) => {
+        if (value > 0) {
+          this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+        }
+      });
     });
-  });
- };
-};
+  }
+
+  move(p) {
+    this.x = p.x;
+    this.y = p.y;
+    this.shape = p.shape;
+  }
+
+  setStartingPosition() {
+    this.x = this.typeId === 4 ? 4 : 3;
+  }
+
+  randomizeTetrominoType(noOfTypes) {
+    return Math.floor(Math.random() * noOfTypes + 1);
+  }
+}
