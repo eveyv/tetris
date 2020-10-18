c20ee23d3cb81f1b7ef750ceb67403bf29e1ec0e
Everett Yeaw <evyeaw@gmail.com>
2020-10-18 11:24:22 -0400

    piece generation upon via onclick on play

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ ctx.canvas.width = COLS * BLOCK_SIZE;
 ctx.canvas.height = ROWS * BLOCK_SIZE;
 
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+
+let board = new Board();
+
+function play() {
+  board.reset();
+  let piece = new Piece(ctx);
+  piece.draw();
+
+  board.piece = piece;
+}
